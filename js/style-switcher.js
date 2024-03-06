38c5b3b07518d862d65d91eb581e5c50f57b9ca2
ARKID-THEGENIUS <143585115+ARKID-THEGENIUS@users.noreply.github.com>
2024-03-06 21:36:37 +0530

    Add files via upload

---
+++ js/style-switcher.js
@@ -0,0 +1,4 @@
+const styleSwitcherToggle =document.querySelector(".style-switcher-toggle");
+styleSwitcherToggle.addEventListener("click", () => {
+    document.querySelector(".style-switcher").classList.toggle("open");
+})
